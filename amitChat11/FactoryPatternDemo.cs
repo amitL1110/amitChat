edbc12e6e6359e9d1f9d150f568a0b504f43bc69
amitL1110 <amit.lax1110@gmail.com>
2023-05-29 15:32:54 +0300

    commit num 4

--- amitChat11/FactoryPatternDemo.cs
+++ amitChat11/FactoryPatternDemo.cs
@@ -10,35 +10,8 @@ public class FactoryPatternDemo
         MessageBase mes1;
         string filePath="hi.pdf";
         //creating MessageBase object just by the extension of the file path
-        if (filePath.Contains((Extensions.Ogg).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.File,Extensions.Ogg,filePath,FileType.Audio);
-        }
-        else if (filePath.Contains((Extensions.Mp3).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.File,Extensions.Mp3,filePath,FileType.Audio);
-        }
-        else if (filePath.Contains((Extensions.Pdf).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.File,Extensions.Pdf,filePath,FileType.Pdf);
-        }
-        else if (filePath.Contains((Extensions.Jpeg).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.Image,Extensions.Jpeg,filePath);
-        }
-        else if (filePath.Contains((Extensions.Png).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.Image,Extensions.Png,filePath);
-        }
-        else if (filePath.Contains((Extensions.Bmp).ToString()))
-        {
-            mes1 = messageFactory.GetMessage(MessageType.Image,Extensions.Bmp,filePath);
-        }
-        else
-        {
-            mes1 = messageFactory.GetMessage(MessageType.Text, filePath);
-        }
-        
+        mes1 = messageFactory.GetMessage(filePath);
+
 
     }
     
--- amitChat11/MessageFactory.cs
+++ amitChat11/MessageFactory.cs
@@ -5,24 +5,37 @@ namespace amitChat11;
 public class MessageFactory
 {
     //use getShape method to get object of type shape 
-    public MessageBase GetMessage(MessageType mesType, string data)
+    public MessageBase GetMessage(string filePath)
     {
-        return new TextMessage(mesType, data);
-    }
+        if (filePath.Contains((Extensions.Ogg).ToString()))
+        {
+            return new AudioFile(MessageType.File, Extensions.Ogg, FileType.Audio, filePath);
+        }
+        else if (filePath.Contains((Extensions.Mp3).ToString()))
+        {
+            return new AudioFile(MessageType.File, Extensions.Mp3, FileType.Audio, filePath);
+        }
+        else if (filePath.Contains((Extensions.Pdf).ToString()))
+        {
+            return new PdfFile(MessageType.File, Extensions.Pdf, FileType.Pdf, filePath);
 
-    public MessageBase GetMessage(MessageType mesType,Extensions extension, string filePath)
-    {
-
-        return new ImageMessage(mesType,filePath,extension);
+        }
+        else if (filePath.Contains((Extensions.Jpeg).ToString()))
+        {
+            return new ImageMessage(MessageType.Image,filePath,Extensions.Jpeg);
+        }
+        else if (filePath.Contains((Extensions.Png).ToString()))
+        {
+            return new ImageMessage(MessageType.Image,filePath,Extensions.Png);
+        }
+        else if (filePath.Contains((Extensions.Bmp).ToString()))
+        {
+            return new ImageMessage(MessageType.Image,filePath,Extensions.Bmp);
+        }
+        else
+        {
+            return new TextMessage(MessageType.Text, filePath);
+        }
     }
     
-    public MessageBase GetMessage(MessageType mesType,Extensions extension, string filePath, FileType fileType)
-    {
-        if (fileType == FileType.Audio)
-        {
-            return new AudioFile(mesType, extension, fileType, filePath);
-        }
-
-        return new PdfFile(mesType, extension, fileType, filePath);
-    }
 }
